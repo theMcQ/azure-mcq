3687a7b4a865946356b487b10349d7dd6feabc2a
McQ Wilson <mike_wilson@ca.ibm.com>
2016-11-18 15:13:39 -0600

    I need to commit

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-﻿var express = require('express');
+/*eslint-env node */
+var express = require('express');
 var app = express();
-var port = process.env.port || 1337
+//var port = process.env.port || 1337
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+var port = process.env.port || appEnv.port;
 
 app.get('/', function (req, res) {
     res.send('Hello World! -- its not very impressive, but it runs');
 });
+
 
 var server = app.listen(port, function () {
     var host = server.address().address;
